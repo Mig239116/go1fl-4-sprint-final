ca39ea1ebe00dba3ba940bd25f1da2c1a8073c52
Mikhail Ignatenko <ignatenkomikhail239@gmail.com>
2025-04-19 13:55:44 +0300

    First review

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,9 +1,24 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	sc "github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
+// Основные виды ошибок.
+var (
+	ErrIncorrectNumberOfArgs = errors.New("неверное количество параметров")
+	ErrStepsNumber           = errors.New("количество шагов должно быть больше нуля")
+	ErrDuration              = errors.New("неверная длительность")
+)
+
+// Основыне виды констант.
 const (
 	// Длина одного шага в метрах
 	stepLength = 0.65
@@ -11,10 +26,52 @@ const (
 	mInKm = 1000
 )
 
+// Переводит данные о тренировке в переменные соответствующих типов.
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	if data == "" {
+		return 0, 0, ErrIncorrectNumberOfArgs
+	}
+	components := strings.Split(data, ",")
+	if len(components) != 2 {
+		return 0, 0, ErrIncorrectNumberOfArgs
+	}
+	steps, err := strconv.Atoi(components[0])
+	if err != nil {
+		return 0, 0, err
+	}
+	if steps <= 0 {
+		return 0, 0, ErrStepsNumber
+	}
+	duration, err := time.ParseDuration(components[1])
+	if duration <= 0 {
+		return 0, 0, ErrDuration
+	}
+	if err != nil {
+		return 0, 0, err
+	}
+	return steps, duration, nil
 }
 
+// DayActionInfo выводит данные об активности за день.
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	if steps <= 0 {
+		log.Println(ErrStepsNumber.Error())
+		return ""
+	}
+	distance := float64(steps) * stepLength
+	distance = distance / mInKm
+	calories, err := sc.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		steps,
+		distance,
+		calories)
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -1,7 +1,21 @@
 package spentcalories
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+)
+
+// Основные виды ошибок.
+var (
+	ErrIncorrectNumberOfArgs = errors.New("неверное количество параметров")
+	ErrStepsNumber           = errors.New("количество шагов должно быть больше нуля")
+	ErrHeight                = errors.New("неверный рост")
+	ErrWeight                = errors.New("неверный вес")
+	ErrDuration              = errors.New("неверная длительность")
+	ErrActivity              = errors.New("неизвестный тип тренировки")
 )
 
 // Основные константы, необходимые для расчетов.
@@ -13,26 +27,115 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе
 )
 
+// Обрабатывает строку с данными о тренировке и возвращает в виде соответствующих типов.
 func parseTraining(data string) (int, string, time.Duration, error) {
-	// TODO: реализовать функцию
+	components := strings.Split(data, ",")
+	if len(components) != 3 {
+		return 0, "", 0, ErrIncorrectNumberOfArgs
+	}
+	steps, err := strconv.Atoi(components[0])
+	if err != nil {
+		return 0, "", 0, err
+	}
+	if steps <= 0 {
+		return 0, "", 0, ErrStepsNumber
+	}
+	duration, err := time.ParseDuration(components[2])
+	if err != nil {
+		return 0, "", 0, err
+	}
+	if duration <= 0 {
+		return 0, "", 0, ErrDuration
+	}
+	if components[1] != "Бег" && components[1] != "Ходьба" {
+		return 0, "", 0, ErrActivity
+	}
+	return steps, components[1], duration, nil
 }
 
+// Переводит количество шагов в дистанцию в км.
 func distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	if steps <= 0 {
+		return 0
+	}
+	return height * stepLengthCoefficient * float64(steps) / mInKm
 }
 
+// Рассчитывает среднюю скорость.
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	}
+	dist := distance(steps, height)
+	return dist / duration.Hours()
 }
 
+// TrainingInfo возвращает данные о тренировке.
 func TrainingInfo(data string, weight, height float64) (string, error) {
-	// TODO: реализовать функцию
+	steps, activity, duration, err := parseTraining(data)
+	if err != nil {
+		return "", err
+	}
+	dist := distance(steps, height)
+	speed := meanSpeed(steps, height, duration)
+	var calories float64
+	var calcErr error
+	switch activity {
+	case "Бег":
+		calories, calcErr = RunningSpentCalories(steps, weight, height, duration)
+	case "Ходьба":
+		calories, calcErr = WalkingSpentCalories(steps, weight, height, duration)
+	default:
+		return "", ErrActivity
+	}
+	if calcErr != nil {
+		return "", err
+	}
+	return fmt.Sprintf(`Тип тренировки: %s
+Длительность: %.2f ч.
+Дистанция: %.2f км.
+Скорость: %.2f км/ч
+Сожгли калорий: %.2f
+`,
+		activity,
+		duration.Hours(),
+		dist,
+		speed,
+		calories), nil
 }
 
+// RunnningSpentCalories рассчитывает калории при беге.
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	calories, err := spentCalories(steps, weight, height, duration)
+	if err != nil {
+		return 0, err
+	}
+	return calories, nil
 }
 
+//WalkingSpentCalories рассчитывает калории при ходьбе.
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	calories, err := spentCalories(steps, weight, height, duration)
+	if err != nil {
+		return 0, err
+	}
+	return calories * walkingCaloriesCoefficient, nil
+}
+
+//Производит расчет калорий.
+func spentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	if steps <= 0 {
+		return 0, ErrStepsNumber
+	}
+	if weight <= 0 {
+		return 0, ErrWeight
+	}
+	if height <= 0 {
+		return 0, ErrHeight
+	}
+	if duration <= 0 {
+		return 0, ErrDuration
+	}
+	speed := meanSpeed(steps, height, duration)
+	return (weight * speed * duration.Minutes()) / minInH, nil
 }
